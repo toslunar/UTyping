448933590c63abe313ae7601aa0ab28331612400
Toshiki Kataoka <tos.lunar@gmail.com>
2007-05-31 19:11:17 +0900

    utkeyinput.h.bak

--- utkeyinput.h
+++ utkeyinput.h
@@ -1,17 +1,89 @@
-#ifndef KEYINPUT_H
-#define KEYINPUT_H
+#ifndef __UTYPING_KEYINPUT
+#define __UTYPING_KEYINPUT
 
 #include "dxlib.h"
-#include <deque>
-using namespace std;
 
-struct KeyboardInput{
+#define KEYBOARD_INPUT_BUFFER_SIZE 256
+
+struct KeyboardInputData{
 	char ch;
 	LONGLONG timeCount;	/* キーが押された時間 */
 };
 
-deque<KeyboardInput> g_deqKeyboardInput;
-//CRITICAL_SECTION g_csKeyboardInput;
+class KeyboardInput{
+public:
+	KeyboardInput();
+	~KeyboardInput();
+	void clear();
+	void push(KeyboardInputData &data);
+	KeyboardInputData pop();
+	bool empty();
+private:
+	void EnterCS();
+	void LeaveCS();
+private:
+	KeyboardInputData m_queue[KEYBOARD_INPUT_BUFFER_SIZE];
+	int m_queueBegin, m_queueEnd;
+	CRITICAL_SECTION m_cs;
+};
+
+KeyboardInput g_keyboardInput;
+/* グローバルで作っておく */
+
+KeyboardInput::KeyboardInput(){
+	InitializeCriticalSection(&m_cs);
+	m_queueEnd = 0;
+	clear();
+}
+
+KeyboardInput::~KeyboardInput(){
+	DeleteCriticalSection(&m_cs);
+}
+
+void KeyboardInput::clear(){
+	m_queueBegin = m_queueEnd;
+}
+
+void KeyboardInput::push(KeyboardInputData &data){
+	EnterCS();
+	if(m_queueEnd - m_queueBegin < KEYBOARD_INPUT_BUFFER_SIZE){
+	/* バッファがいっぱいではない */
+		m_queue[m_queueEnd % KEYBOARD_INPUT_BUFFER_SIZE] = data;
+		m_queueEnd++;
+	}
+	LeaveCS();
+}
+
+KeyboardInputData KeyboardInput::pop(){
+	EnterCS();
+	KeyboardInputData data;
+	data = m_queue[m_queueBegin % KEYBOARD_INPUT_BUFFER_SIZE];
+	m_queueBegin++;
+	LeaveCS();
+	return data;
+}
+
+bool KeyboardInput::empty(){
+	bool isEmpty;
+	EnterCS();
+	if(m_queueBegin == m_queueEnd){
+		isEmpty = true;
+	}else{
+		isEmpty = false;
+	}
+	LeaveCS();
+	return isEmpty;
+}
+
+void KeyboardInput::EnterCS(){
+	EnterCriticalSection(&m_cs);
+}
+
+void KeyboardInput::LeaveCS(){
+	LeaveCriticalSection(&m_cs);
+}
+
+/* ============================================================ */
 
 long FAR PASCAL KeyboardInputProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam){
 	static char CtrlCode[][ 2 ] =
@@ -31,11 +103,11 @@ long FAR PASCAL KeyboardInputProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM
 		{
 			// 文字をバッファにコピー
 			//ST_StokInputChar( ( char )wParam ) ;
-			KeyboardInput ki;
-			ki.ch = (char)wParam;
-			ki.timeCount = GetNowHiPerformanceCount();
-//printfDx("%c(%d), %f\n", ki.ch, ki.ch, (double)ki.timeCount);
-			g_deqKeyboardInput.push_back(ki);
+			KeyboardInputData data;
+			data.ch = (char)wParam;
+			data.timeCount = GetNowHiPerformanceCount();
+//printfDx("%c(%d), %f\n", data.ch, data.ch, (double)data.timeCount);
+			g_keyboardInput.push(data);
 		}
 		break ;
 	case WM_KEYDOWN:
@@ -51,11 +123,11 @@ long FAR PASCAL KeyboardInputProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM
 				{
 					// バッファに文字コードを代入
 					//ST_StokInputChar( *( CCode + 1 ) ) ;
-					KeyboardInput ki;
-					ki.ch = *(CCode + 1);
-					ki.timeCount = GetNowHiPerformanceCount();
-//printfDx("%c(%d), %f\n", ki.ch, ki.ch, (double)ki.timeCount);
-					g_deqKeyboardInput.push_back(ki);
+					KeyboardInputData data;
+					data.ch = *(CCode + 1);
+					data.timeCount = GetNowHiPerformanceCount();
+//printfDx("%c(%d), %f\n", data.ch, data.ch, (double)data.timeCount);
+					g_keyboardInput.push(data);
 					break ;
 				}
 				CCode += 2 ;
@@ -69,26 +141,24 @@ long FAR PASCAL KeyboardInputProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM
 /* キーを押した時刻を必要としない（返さない）バージョン */
 char GetKeyboardInput(){
 	char ch;
-	if(g_deqKeyboardInput.empty()){	/* キューが空 */
+	if(g_keyboardInput.empty()){	/* キューが空 */
 		ch = 0;
 	}else{	/* キューが空でないのでそれを取得 */
-		KeyboardInput &ki = g_deqKeyboardInput.front();
-		ch = ki.ch;
-		//timeCount = ki.timeCount;
-		g_deqKeyboardInput.pop_front();	/* 読んだら削除 */
+		KeyboardInputData data = g_keyboardInput.pop();
+		ch = data.ch;
+		//timeCount = data.timeCount;
 	}
 	return ch;
 }
 
 char GetKeyboardInput(LONGLONG &timeCount){
 	char ch;
-	if(g_deqKeyboardInput.empty()){	/* キューが空 */
+	if(g_keyboardInput.empty()){	/* キューが空 */
 		ch = 0;
 	}else{	/* キューが空でないのでそれを取得 */
-		KeyboardInput &ki = g_deqKeyboardInput.front();
-		ch = ki.ch;
-		timeCount = ki.timeCount;
-		g_deqKeyboardInput.pop_front();	/* 読んだら削除 */
+		KeyboardInputData data = g_keyboardInput.pop();
+		ch = data.ch;
+		timeCount = data.timeCount;
 	}
 	return ch;
 }
